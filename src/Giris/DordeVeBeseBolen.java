719484928ae5a3dfc153cd5ea6dacf999ccf9ed3
Tunc Cenk Erguven <erguven.cenk@gmail.com>
2022-07-26 17:14:21 +0300

    4 ve 5'in katlarını bulma programı eklendi.

--- src/Giris/DordeVeBeseBolen.java
+++ src/Giris/DordeVeBeseBolen.java
@@ -1,6 +1,6 @@
 package Giris;
 import java.util.Scanner;
-public class GirilenSayi {
+public class DordeVeBeseBolen {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter your number : ");
