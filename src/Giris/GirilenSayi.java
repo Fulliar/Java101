2ebf5176d839219a3aa494bf566e177f153de9e2
Tunc Cenk Erguven <erguven.cenk@gmail.com>
2022-07-26 17:12:36 +0300

    4 ve 5'in katlarını bulma programı eklendi.

---
+++ src/Giris/GirilenSayi.java
@@ -0,0 +1,25 @@
+package Giris;
+import java.util.Scanner;
+public class GirilenSayi {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your number : ");
+        int n = in.nextInt();
+        System.out.println("Multiples of 4 : ");
+
+        for(int i=1; i <= n ; i++ ){
+
+            if(i%4== 0){
+                System.out.println( i);
+            }
+        }
+        System.out.println("***********************************");
+        System.out.println("Multiples of 5 : ");
+        for(int i=1; i <= n ; i++ ){
+            if(i%5 == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
